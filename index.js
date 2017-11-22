9aff6198250424e1480b8efe6f54ae8a11706d09
Louis Chatriot <louis.chatriot@gmail.com>
2017-11-22 23:02:02 +0100

    Can get the sum of absolute differences

--- index.js
+++ index.js
@@ -1,27 +1,91 @@
 const Gdax = require('gdax');
-const concat = require('concat-stream')
+const concat = require('concat-stream');
+const Writable = require('stream').Writable;
+const Transform = require('stream').Transform;
 
 const publicClient = new Gdax.PublicClient("btc-eur");
 
-// Promise API actually still not released
-//publicClient.getProducts().then(products => { console.log(products) });
 
-//publicClient.getProductTrades({ limit: 5 }, function (err, res, trades) {
-  //console.log(trades);
-//});
-
-
-// ~10k latest trades
-var trades = publicClient.getProductTradeStream(5560619,5560622);
-
-
-
-var concatStream = concat(receivedData)
+var trades = publicClient.getProductTradeStream(5560405,5560622);
 trades.on('error', err => { console.log(err); process.exit(1); })
-trades.pipe(concatStream)
+trades.on('end', noMoreTrades);
+
+//var timeSeries = [];
+//var currentDirection;
+//var previousTrade;
+//var timeSeriesStream = Transform({ objectMode: true });
+//timeSeriesStream._transform = function (trade, enc, next) {
+  //console.log("======================");
+  ////console.log(trade);
+
+  //if (currentDirection === undefined) { previousTrade = trade; }   // First trade is always recorded
+
+  //if (currentDirection === undefined || currentDirection !== trade.side) {
+    //currentDirection = trade.side;
+    //timeSeries.push({ price: previousTrade.price, time: previousTrade.time });   // Recording the previous trade once we change directions
+    //this.push({ price: previousTrade.price, time: previousTrade.time });
+  //}
+
+  //previousTrade = trade;
+  //next();
+//}
 
 
 
-function receivedData(data) {
-  console.log(data);
+var timeSeries = [];
+var currentDirection;
+var previousTrade;
+
+var timeSeriesStream = Writable({ objectMode: true });
+timeSeriesStream._write = function (trade, enc, next) {
+  console.log("======================");
+  console.log(trade);
+
+  if (currentDirection === undefined) {
+    previousTrade = trade;   // First trade is always recorded
+  }
+
+  if (currentDirection === undefined || currentDirection !== trade.side) {
+    currentDirection = trade.side;
+    timeSeries.push({ price: previousTrade.price, time: previousTrade.time });   // Recording the previous trade once we change directions
+  }
+
+  previousTrade = trade;
+  next();
 }
+
+
+
+
+
+trades.pipe(timeSeriesStream);
+
+function noMoreTrades () {
+  console.log("======================================");
+  console.log("===~ No more trades to be handled ~===");
+  console.log(timeSeries);
+  console.log("======================================");
+
+  var sumOfPriceIncreases = 0;
+  var min = max = timeSeries[0].price;
+  for (var i = 0; i < timeSeries.length - 1; i += 1) {
+    //console.log(typeof timeSeries[i].price);
+    //console.log(timeSeries[i].price);
+    sumOfPriceIncreases += Math.max(0, timeSeries[i+1].price - timeSeries[i].price);
+    min = Math.min(min, timeSeries[i + 1].price);
+    max = Math.max(max, timeSeries[i + 1].price);
+  }
+
+  console.log("Sum of all price increases: " + sumOfPriceIncreases);
+  console.log("Minimum price of the period: " + min);
+  console.log("Maximum price of the period: " + max);
+  console.log("Delta between minimum and maximum on the period: " + (max - min));
+}
+
+
+
+
+
+
+
+
