ad463b9bcde536926288a98ec35549c153bef3a5
Louis Chatriot <louis.chatriot@gmail.com>
2017-11-26 22:02:48 +0100

    Some more stats

--- analize-movements.js
+++ analize-movements.js
@@ -5,7 +5,7 @@ const movements = new Nedb({ autoload: true
                             });
 
 movements.find({}).sort({ date: 1 }).exec(function (err, docs) {
-  var gain = 0, thisTrade;
+  var gain = 0, thisTrade, min = docs[0].price, max = docs[0].price;
 
   docs.forEach(function (m) {
     if (thisTrade === undefined && m.direction === 'up') {
@@ -14,9 +14,20 @@ movements.find({}).sort({ date: 1 }).exec(function (err, docs) {
       gain += m.price - thisTrade;
       thisTrade = undefined;
     }
+
+    min = Math.min(min, m.price);
+    max = Math.max(max, m.price);
   });
 
   console.log("Absolute gain on trading on one bitcoin: " + gain);
   console.log("Relative gain: " + (gain / docs[0].price));
+  console.log("================================================");
+  console.log("Beginning: " + docs[0].price);
+  console.log("End: " + docs[docs.length - 1].price);
+  console.log("Delta: " + (docs[docs.length - 1].price - docs[0].price));
+  console.log("================================================");
+  console.log("Minimum: " + min);
+  console.log("Maximum: " + min);
+  console.log("Delta: " + (max - min));
 });
 
